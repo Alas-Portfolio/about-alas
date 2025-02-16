11e51a26a9842ad8480021bc024fe8b0ca43dd40
Ferrer, Alastair <162070969+AlastairFerrer@users.noreply.github.com>
2025-02-16 19:23:56 +0800

    --vue-app

---
+++ about-alas/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <nav>
+           <router-link to="/Home">Home</router-link> 
+           <router-link to="/About">Contact</router-link> 
+           <router-link to="/Contact">About</router-link> 
+        </nav>
+    </header>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
